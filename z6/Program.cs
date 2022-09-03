d3d19414e789af6a62e21156734d6c35916af30a
AlexanderGonchar <goncharas24@gmail.com>
2022-09-03 08:14:37 +0300

    add z6

--- z6/Program.cs
+++ z6/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
 
-int[] arr = {1,1,0,0,0,1,1,0};
-for(int i=0; i < arr.Length; i++)
+int[] arr = { 1, 1, 0, 0, 0, 1, 1, 0 };
+for (int i = 0; i < arr.Length; i++)
 {
     Console.Write(arr[i]);
 }
